6cdffd2599532cd3c76f3859e6f4638b7bbc94d0
cjmodi306 <cjmodi306@gmail.com>
2026-02-11 11:15:44 +0100

    main project

---
+++ 01_introduction_to_self_driving_cars/module_5_vehicle_longitudinal_control/test.py
@@ -0,0 +1,6 @@
+# Source - https://stackoverflow.com/a
+# Posted by NorthCat, modified by community. See post 'Timeline' for change history
+# Retrieved 2025-12-30, License - CC BY-SA 4.0
+
+import pdfkit
+pdfkit.from_url('Module 5 Graded Quiz _ Coursera.html', 'out.pdf')
